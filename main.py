8a19ade310768637a1e56e53fa3cf91b7d864934
Muneeb Ahmed <ahmed4507537@cloud.neduet.edu.pk>
2024-09-13 15:49:14 +0500

    init

---
+++ main.py
@@ -0,0 +1,36 @@
+import env
+import sensors
+import pygame
+
+
+environment = env.buildEnvironment((600, 1200))
+environment.originalMap = environment.map.copy()
+laser = sensors.LaserSensor(200, environment.originalMap, uncertainity=(0.5,0.01))
+environment.map.fill((0,0,0))
+environment.infomap = environment.map.copy()
+
+running = True
+
+while running:
+    sensorON = False
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        if pygame.mouse.get_focused():
+            sensorON = True
+        elif not pygame.mouse.get_focused():
+            sensorON = False
+    
+    if sensorON:
+        position = pygame.mouse.get_pos()
+        laser.position = position
+        sensor_data = laser.sense()
+        environment.dataStorage(sensor_data)
+        environment.render()
+    environment.map.blit(environment.infomap, (0, 0))
+    pygame.display.update()
+pygame.quit()
+
+    
+
+
